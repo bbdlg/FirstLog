ec2dd78e1f5f58d309adedade131f36b5d44ab68
bbdlg <shiluo.110@163.com>
2013-04-01 17:46:55 +0800

    Add photo view in log, fix bugs.

--- FirstLog/src/com/example/firstlog/LogListAdapter.java
+++ FirstLog/src/com/example/firstlog/LogListAdapter.java
@@ -3,10 +3,13 @@ package com.example.firstlog;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
@@ -44,15 +47,28 @@ public class LogListAdapter extends BaseAdapter {
 		// TODO Auto-generated method stub
         LayoutInflater inflater = context.getLayoutInflater();  
         View itemView = inflater.inflate(R.layout.list_item, null);  
-        UserData info = list.get(position);  
+        final UserData info = list.get(position);  
         String sort = info.getSort();
-        TextView   textView = (TextView) itemView.findViewById(R.id.list_item_textview);  
+        TextView  textView 	= (TextView) itemView.findViewById(R.id.list_item_textview);  
         ImageView imageView = (ImageView)itemView.findViewById(R.id.list_image);  
         textView.setText(info.getContent()); 
         if(sort.equals(UserData.PHOTO)) {
         	Bitmap bmpTmp = FirstLogHelper.getImageThumbnail((FirstLogHelper.localRootPath+"/"+info.getContent()), 200, 200); 
         	Log.i("LogListAdapter", "set bmp path:<"+FirstLogHelper.localRootPath+"/"+info.getContent()+">");
         	imageView.setImageBitmap(bmpTmp);
+        	imageView.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					Intent intent = new Intent();
+					Bundle bundle = new Bundle();
+    				bundle.putString("imagePath", FirstLogHelper.localRootPath+"/"+info.getContent());
+    				intent.putExtras(bundle);
+					intent.setClass(context, PreviewImageActivity.class);
+					context.startActivity(intent);
+				}
+			});
         }
           
         return itemView;  
