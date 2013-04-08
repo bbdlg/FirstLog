67f07ad18ad1385edab6aac5c69f810d1a3e2699
bbdlg <shiluo.110@163.com>
2013-04-08 17:37:21 +0800

    Add lbs container.

--- FirstLog/src/com/example/firstlog/LogTextActivity.java
+++ FirstLog/src/com/example/firstlog/LogTextActivity.java
@@ -9,6 +9,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class LogTextActivity extends Activity {
 
@@ -17,6 +18,17 @@ public class LogTextActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); 
         setContentView(R.layout.log_text);
+        
+        //Location Based Service，LBS
+        ImageView lbs = (ImageView)findViewById(R.id.imageView_lbs);
+        lbs.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
         
         //save
         ImageButton saveButton = (ImageButton)findViewById(R.id.imageButton_save);
