0217a29e4e4b712b7c6c3d37b35efb9b0de5ac20
DEXIN3 <20043790@163.com>
2013-08-13 16:31:14 +0800

    add about activity.

--- FirstLog/src/com/example/firstlog/GuideActivity.java
+++ FirstLog/src/com/example/firstlog/GuideActivity.java
@@ -36,67 +36,6 @@ public class GuideActivity extends Activity {
              dirFirstFile.mkdir(); 
         }
         
-        //get location
-/*
-        LocationManager locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-        LocationListener locationListener = new LocationListener() {
-			
-			@Override
-			public void onStatusChanged(String provider, int status, Bundle extras) {
-				//dialogTips("status changed");
-			}
-			
-			@Override
-			public void onProviderEnabled(String provider) {
-				//dialogTips(provider+" enable");
-			}
-			
-			@Override
-			public void onProviderDisabled(String provider) {
-				//dialogTips(provider+" disable");
-			}
-			
-			@Override
-			public void onLocationChanged(Location location) {
-				// TODO Auto-generated method stub
-				Log.i("gps", "call onLocationChanged()");
-				((FirstLogHelper)getApplication()).setLocation(location);
-			}
-		};
-		// 判断Use GPS satellites.是否勾选
-        boolean isGpsEnabled = locationManager.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER);
-        // 判断Use wireless networks 是否勾选。因该函数一直返回true，故改用其他方式
-        //boolean isNetworkEnabled = locationManager.isProviderEnabled(android.location.LocationManager.NETWORK_PROVIDER);
-        boolean isNetworkEnabled = false;
-        ConnectivityManager cwjManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (cwjManager.getActiveNetworkInfo() != null) {
-        	isNetworkEnabled = cwjManager.getActiveNetworkInfo().isAvailable();
-        }
-        
-        Log.i("haha", "GPS:"+isGpsEnabled+", Network:"+isNetworkEnabled);
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        
-        if(!isGpsEnabled && !isNetworkEnabled) {
-        	//tips
-        	//showDialog("");
-        	Log.i("haha", "did't set GPS or network!");
-        	((FirstLogHelper)getApplication()).dialogTips(GuideActivity.this, "没有网络没有GPS让我咋知道您现在在哪猫着呢？=_=");
-			Log.w("diag", "call diaglogConfirm in guideActivity() ... ");
-        }
-
-        //get last known location
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if(null == location) {
-        	Log.i("Location", "can't get location by gps, now via network");
-        	location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        }
-        if(null == location) {
-        	Log.i("Location", "can't get location by network, now set a default value");
-        	location = new Location(LocationManager.NETWORK_PROVIDER);
-        }
-        ((FirstLogHelper)getApplication()).setLocation(location);
- */       
         //start text
         Button textButton = (Button)findViewById(R.id.button_startlog);
         textButton.setOnClickListener(new OnClickListener() {
@@ -109,97 +48,6 @@ public class GuideActivity extends Activity {
 				startActivity(intent);
 			}
 		});      
-
-        /*
-        //start audio
-        Button audioButton = (Button)findViewById(R.id.startaudio);
-        
-        audioButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				if(false == startRecordAudio) {
-					startRecordAudio = true;
-					Button button = (Button)findViewById(R.id.startaudio);
-					button.setText("说完啦");
-				}
-				else {
-					startRecordAudio = false;
-					Button button = (Button)findViewById(R.id.startaudio);
-					button.setText(R.string.guide_audio);
-				}
-			}
-		}); 
-        
-        //start photo
-        Button photoButton = (Button)findViewById(R.id.startphoto);
-        photoButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-
-				String state = Environment.getExternalStorageState();
-				if (state.equals(Environment.MEDIA_MOUNTED)) {
-					file = new File(saveDir, "temp.jpg");
-					file.delete();
-					if (!file.exists()) {
-						try {
-							file.createNewFile();
-						} catch (IOException e) {
-							e.printStackTrace();
-							Toast.makeText(GuideActivity.this, "照片文件创建失败了，是什么原因呢？",
-									Toast.LENGTH_LONG).show();
-							return;
-						}
-					}
-					Intent intent = new Intent(
-							"android.media.action.IMAGE_CAPTURE");
-					intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-					startActivityForResult(intent, UserData.ENUM_PHOTO);
-				} else {
-					Toast.makeText(GuideActivity.this, "您老的sdcard坏了或者没插~",
-							Toast.LENGTH_SHORT).show();
-				}
-			
-			}
-		});   
- 
-        //start video
-        Button videoButton = (Button)findViewById(R.id.startvideo);
-        videoButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				
-				String state = Environment.getExternalStorageState();
-				if (state.equals(Environment.MEDIA_MOUNTED)) {
-					file = new File(saveDir, "temp.mp4");
-					file.delete();
-					if (!file.exists()) {
-						try {
-							file.createNewFile();
-						} catch (IOException e) {
-							e.printStackTrace();
-							Toast.makeText(GuideActivity.this, "录像文件创建失败了，是什么原因呢？",
-									Toast.LENGTH_LONG).show();
-							return;
-						}
-					}
-					Intent intent = new Intent(
-							"android.media.action.VIDEO_CAPTURE");
-					intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-					startActivityForResult(intent, UserData.ENUM_VIDEO);
-				} else {
-					Toast.makeText(GuideActivity.this, "您老的sdcard坏了或者没插~",
-							Toast.LENGTH_SHORT).show();
-				}
-			
-			}
-		}); 
-		*/
         
         //show log
         Button showlogButton = (Button)findViewById(R.id.button_showlog);
@@ -232,11 +80,12 @@ public class GuideActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-    	menu.add(Menu.NONE, Menu.FIRST + 1, 1, "换个账号Log First").setIcon(android.R.drawable.ic_menu_delete);
-    	menu.add(Menu.NONE, Menu.FIRST + 2, 2, "上传本地数据到云端").setIcon(android.R.drawable.ic_menu_delete);
-    	menu.add(Menu.NONE, Menu.FIRST + 3, 3, "下载云端数据到本地").setIcon(android.R.drawable.ic_menu_call);
+    	menu.add(Menu.NONE, Menu.FIRST + 1, 1, "切换账号");
+    	menu.add(Menu.NONE, Menu.FIRST + 2, 2, "上传数据");
+    	menu.add(Menu.NONE, Menu.FIRST + 3, 3, "下载数据");
+    	menu.add(Menu.NONE, Menu.FIRST + 4, 4, "关于");
 
-       return true;
+    	return true;
     }
     
     @Override
@@ -322,7 +171,13 @@ public class GuideActivity extends Activity {
 			}
 			
         	break;
-
+        	
+        case Menu.FIRST + 4:	//about
+        	Intent aboutIntent = new Intent();
+        	aboutIntent.setClass(GuideActivity.this, AboutActivity.class);
+        	startActivity(aboutIntent);
+        
+        	break;
         }
         
         return false;
@@ -330,60 +185,7 @@ public class GuideActivity extends Activity {
     
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-/*		
-		super.onActivityResult(requestCode, resultCode, data);
-		
-		if(RESULT_OK != resultCode) {
-			Log.e("GuideActivity", "Other activity return error code:"+resultCode);
-			return;
-		}
 
-		String time = ((FirstLogHelper)getApplication()).getTime();
-		String name = ""+time;
-		String sort = "";
-				
-		switch(requestCode) {
-			case UserData.ENUM_AUDIO:
-				name += ".mp3";
-				sort = UserData.AUDIO;
-				break;
-	
-			case UserData.ENUM_PHOTO:
-				name += ".jpg";
-				sort = UserData.PHOTO;
-				break;
-				
-			case UserData.ENUM_VIDEO:
-				name += ".mp4";
-				sort = UserData.VIDEO;
-				break;
-		}
-		
-		if (file != null && file.exists()) {
-			//store in sdcard
-			String yearAndMonth = ((FirstLogHelper)getApplication()).getYearAndMonth();
-			File newfile = new File(saveDir+"/"+yearAndMonth, name);
-			File savePath = new File(saveDir+"/"+yearAndMonth);
-			if (!savePath.exists()) {
-				savePath.mkdirs();
-			}
-			file.renameTo(newfile);
-			file.delete();
-			
-	        //store in db
-			SharedPreferences statusPreferences = getSharedPreferences("firstlog", 0);
-	        String email = statusPreferences.getString("username", "noSuchEmailUser");
-			UserData userdata = new UserData();
-			userdata.setEmail(email);
-			userdata.setTimesec(""+time);
-			userdata.setLongitude(""+((FirstLogHelper)getApplication()).getLocation().getLongitude());
-			userdata.setLatitude(""+((FirstLogHelper)getApplication()).getLocation().getLatitude());
-			userdata.setSort(sort);
-			userdata.setContent(yearAndMonth+"/"+name);
-			
-			UserDataHelper userDataHelper = new UserDataHelper(GuideActivity.this);
-			userDataHelper.saveUserData(userdata);
-		}*/
 	}
 	
 	
