cce6387b110e668ae612cbfaf61ae113517237c5
DEXIN3 <20043790@163.com>
2013-08-19 23:45:38 +0800

    modified about activity show style; Program version will be decided by @string/version now.

--- FirstLog/src/com/bbdlg/firstlog/AboutActivity.java
+++ FirstLog/src/com/bbdlg/firstlog/AboutActivity.java
@@ -1,115 +1,69 @@
 package com.bbdlg.firstlog;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.KeyEvent;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 	
-	public final static String urlAbout = "http://m.firstlog.net/about_firstlog";
+	Button btShare;
+	Button btGood;
+	TextView tvUrl;
+
 	
-	/** Called when the activity is first created. */
-	WebView wv;
-	ProgressDialog pd;
-	Handler handler;
-
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);		
 		setContentView(R.layout.about);
-		init();// 执行初始化函数
-		loadurl(wv, urlAbout);
-		handler = new Handler() {
-			public void handleMessage(Message msg) {// 定义一个Handler，用于处理下载线程与UI间通讯
-				if (!Thread.currentThread().isInterrupted()) {
-					switch (msg.what) {
-					case 0:
-						pd.show();// 显示进度对话框
-						break;
-					case 1:
-						pd.hide();// 隐藏进度对话框，不可使用dismiss()、cancel(),否则再次调用show()时，显示的对话框小圆圈不会动。
-						break;
-					}
-				}
-				super.handleMessage(msg);
-			}
-		};
-	}
-
-	public void init() {// 初始化
-		wv = (WebView) findViewById(R.id.wv);
-		wv.getSettings().setJavaScriptEnabled(true);// 可用JS
-		wv.setScrollBarStyle(0);// 滚动条风格，为0就是不给滚动条留空间，滚动条覆盖在网页上
-		wv.setWebViewClient(new WebViewClient() {
-			public boolean shouldOverrideUrlLoading(final WebView view,
-					final String url) {
-				loadurl(view, url);// 载入网页
-				return true;
-			}// 重写点击动作,用webview载入
-
-		});
-		wv.setWebChromeClient(new WebChromeClient() {
-			public void onProgressChanged(WebView view, int progress) {// 载入进度改变而触发
-				if (progress == 100) {
-					handler.sendEmptyMessage(1);// 如果全部载入,隐藏进度对话框
-				}
-				super.onProgressChanged(view, progress);
-			}
-		});
-
-		pd = new ProgressDialog(AboutActivity.this);
-		pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-		pd.setMessage("数据载入中，请稍候！");
-	}
-
-	public boolean onKeyDown(int keyCode, KeyEvent event) {// 捕捉返回键
-		if ((keyCode == KeyEvent.KEYCODE_BACK) && wv.canGoBack()) {
-			wv.goBack();
-			return true;
-		} else if (keyCode == KeyEvent.KEYCODE_BACK) {
-//			ConfirmExit();// 按了返回键，但已经不能返回，则执行退出确认
-			AboutActivity.this.finish();// 关闭activity
-			return true;
-		}
-		return super.onKeyDown(keyCode, event);
-	}
-
-	public void ConfirmExit() {// 退出确认
-		AlertDialog.Builder ad = new AlertDialog.Builder(AboutActivity.this);
-		ad.setTitle("退出");
-		ad.setMessage("是否退出软件?");
-		ad.setPositiveButton("是", new DialogInterface.OnClickListener() {// 退出按钮
-					@Override
-					public void onClick(DialogInterface dialog, int i) {
-						// TODO Auto-generated method stub
-						AboutActivity.this.finish();// 关闭activity
-
-					}
-				});
-		ad.setNegativeButton("否", new DialogInterface.OnClickListener() {
+		
+		btShare = (Button)findViewById(R.id.bt_share);
+		btGood = (Button)findViewById(R.id.bt_good);
+		tvUrl = (TextView)findViewById(R.id.url);
+		
+		btShare.setOnClickListener(new OnClickListener() {
+			
 			@Override
-			public void onClick(DialogInterface dialog, int i) {
-				// 不退出不用执行任何操作
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				 Intent intent=new Intent(Intent.ACTION_SEND);
+			      
+			      intent.setType("text/plain");
+			      intent.putExtra(Intent.EXTRA_SUBJECT, "分享");
+			      intent.putExtra(Intent.EXTRA_TEXT, "嗨，有一款软件推荐给你：FirstLog，可以记录生活中的各种第一次。它有很强的生命力，正在不断的吸收用户反馈，积极完善自己。快点试用一下吧，在应用商店搜索FirstLog即可。");
+			      startActivity(Intent.createChooser(intent, getTitle()));
 			}
 		});
-		ad.show();// 显示对话框
-	}
+		
+		btGood.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
 
-	public void loadurl(final WebView view, final String url) {
-		new Thread() {
-			public void run() {
-				handler.sendEmptyMessage(0);
-				view.loadUrl(url);// 载入网页
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse("market://details?id=" + getPackageName()));
+//				intent.setData(Uri.parse("market://search?q=RT"));
+				startActivity(intent);
 			}
-		}.start();
+		});
+		
+		tvUrl.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Uri uri = Uri.parse(getResources().getString(R.string.about_url));  
+				Intent it = new Intent(Intent.ACTION_VIEW, uri);  
+				startActivity(it);
+			}
+		});
+		
 	}
 
 }
--- FirstLog/src/com/bbdlg/firstlog/GuideActivity.java
+++ FirstLog/src/com/bbdlg/firstlog/GuideActivity.java
@@ -58,21 +58,17 @@ public class GuideActivity extends Activity {
     View menuView;
     /** 菜单图片 **/
     int[] menu_image_array = {
-    		R.drawable.menu_upload, R.drawable.menu_download, R.drawable.menu_change_user,
-    		R.drawable.menu_good, R.drawable.menu_share, R.drawable.menu_info
+    		R.drawable.menu_upload, R.drawable.menu_download, R.drawable.menu_change_user,R.drawable.menu_info
     };
     /** 菜单文字 **/
     String[] menu_name_array = { 
-    		"上传数据", "下载数据", "切换账号",
-    		"评价", "分享", "关于"
+    		"上传数据", "下载数据", "切换账号","关于"
     };
     /** 宏定义 **/
     private final static int MENU_UPLOAD = 0;
     private final static int MENU_DOWNLOAD = 1;
     private final static int MENU_CHANGEUSER = 2;
-    private final static int MENU_GOOD = 3;
-    private final static int MENU_SHARE = 4;
-    private final static int MENU_INFO = 5;
+    private final static int MENU_INFO = 3;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -221,27 +217,9 @@ public class GuideActivity extends Activity {
                 	Intent aboutIntent = new Intent();
                 	aboutIntent.setClass(GuideActivity.this, AboutActivity.class);
                 	startActivity(aboutIntent);
+//                	overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                 
                 	break;
-                	
-            	case MENU_SHARE:
-					Intent intentShare = new Intent(Intent.ACTION_SEND);
-
-					intentShare.setType("text/plain");
-					intentShare.putExtra(Intent.EXTRA_SUBJECT, "分享");
-					intentShare.putExtra(Intent.EXTRA_TEXT,
-							"嗨，有一款软件推荐给你：FirstLog，可以记录生活中的各种第一次。它有很强的生命力，正在不断的吸收用户反馈，积极完善自己。快点试用一下吧。");
-					startActivity(Intent.createChooser(intentShare, getTitle()));                	
-            
-					break;
-					
-            	case MENU_GOOD:
-            		Intent intentGood = new Intent(Intent.ACTION_VIEW);
-    				intentGood.setData(Uri.parse("market://details?id=" + getPackageName()));
-//    				intent.setData(Uri.parse("market://search?q=RT"));
-    				startActivity(intentGood);
-    				
-    				break;
 					
                 default:
                 	Log.d("debug", "arg2 = "+arg2);
@@ -361,95 +339,12 @@ public class GuideActivity extends Activity {
         return false;// 返回为true 则显示系统menu
     }
     
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//        case Menu.FIRST + 1:	//change user id
-//            break;
-            
-//        case Menu.FIRST + 2:	//upload files and sync data
-//        	MessageBox msgBox =  new MessageBox(this);
-//	        int ret = msgBox.showDialog("上传数据可能产生较大流量，建议在wifi网络下进行。", "温馨提示");
-//	    	if(0 == ret) {
-//	    		break;
-//	    	}
-//	    	
-//            SharedPreferences statusPreferences = getSharedPreferences("firstlog", 0);
-//            String curuser = statusPreferences.getString("username", "noSuchEmailUser");
-//            String token = statusPreferences.getString("token_of_"+curuser, "no_token");
-//			if(token.equals("no_token")) {
-//				Log.e("sync", "token has not found");
-//				Toast.makeText(this, "当前账户未授权，不妨换个账号登录试试~", Toast.LENGTH_LONG).show();
-//			}
-//			else {				
-//				FirstLogHelper.token = token;
-//				
-//				Log.w("sync", "start sync ...");
-//				Toast.makeText(this, "开始上传本地文件到云端，并同步logs...", Toast.LENGTH_LONG).show();
-//				
-//				FileSyncHelper fileSyncHelper = new FileSyncHelper(GuideActivity.this);
-//				fileSyncHelper.syncFiles("upload");
-//				
-//				TableSyncHelper tableSyncHelper = new TableSyncHelper(GuideActivity.this, curuser);
-//				tableSyncHelper.syncTables();
-//				
-//				//Log.w("sync", "finish sync!");
-//				//Toast.makeText(this, "恭喜恭喜！本地文件上传完成，logs同步完成~", Toast.LENGTH_LONG).show();
-//			}
-//
-//            break;
-//            
-//        case Menu.FIRST + 3:	//download files
-//        	//((FirstLogHelper)getApplication()).dialogConfirm(GuideActivity.this, "本操作将下载云端所有的照片、视频、语音等，可能占用大量空间，请慎重:)");
-//	        MessageBox msgBox1 =  new MessageBox(this);
-//	        int ret1 = msgBox1.showDialog("下载数据可能产生较大流量，建议在wifi网络下进行。同时如果您使用了较长时间的firstlog，本操作还可能会占用您本地SD卡的较大空间，请慎重", "温馨提示");
-//        	if(0 == ret1) {
-//        		break;
-//        	}
-//	        
-//            SharedPreferences statusPreferences1 = getSharedPreferences("firstlog", 0);
-//            String curuser1 = statusPreferences1.getString("username", "noSuchEmailUser");
-//            String token1 = statusPreferences1.getString("token_of_"+curuser1, "no_token");
-//			if(token1.equals("no_token")) {
-//				Log.e("sync", "token has not found");
-//				Toast.makeText(this, "当前账户未授权，不妨换个账号登录试试~", Toast.LENGTH_LONG).show();
-//			}
-//			else {				
-//				FirstLogHelper.token = token1;
-//				
-//				Log.w("sync", "start sync ...");
-//				Toast.makeText(this, "开始从云端下载文件...", Toast.LENGTH_LONG).show();
-//				
-//				FileSyncHelper fileSyncHelper = new FileSyncHelper(GuideActivity.this);
-//				fileSyncHelper.syncFiles("download");
-//				
-//				//Log.w("sync", "finish sync!");
-//				//Toast.makeText(this, "恭喜恭喜！云端的所有文件均已下载到本地~", Toast.LENGTH_LONG).show();
-//			}
-//			
-//        	break;
-//        	
-//        case Menu.FIRST + 4:	//about
-//        	Intent aboutIntent = new Intent();
-//        	aboutIntent.setClass(GuideActivity.this, AboutActivity.class);
-//        	startActivity(aboutIntent);
-//        
-//        	break;
-//        }
-//        
-//        return false;
-//    }
-    
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-	}
 	
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 	    if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){   
 	        if((System.currentTimeMillis()-exitTime) > 2000){  
-	            Toast.makeText(getApplicationContext(), "再按一次退出程序~\n如果正在上传或下载数据，建议您稍候再退出 :)", Toast.LENGTH_SHORT).show();                                
+	            Toast.makeText(getApplicationContext(), "再按一次退出程序~", Toast.LENGTH_SHORT).show();                                
 	            exitTime = System.currentTimeMillis();   
 	        } else {
 //	            finish();
